85e178168ab87798e285f6d49d2dabb52f624d83
Jan Schulte <hello@unexpected-co.de>
2014-06-27 21:28:16 +0200

    Use shell script for provisioning
    Installs:
    -git
    -curl
    -rvm
    -ruby 2.1

---
+++ provision-script.sh
@@ -0,0 +1,25 @@
+#/bin/bash --login
+
+# At first, update package sources
+sudo apt-get update
+
+#-y means 'assumes yes', so it does not ask
+# 'Do you want to install? [y,n]
+
+#First install curl
+sudo apt-get install -y curl
+#Then git
+sudo apt-get install -y git
+
+# Download and install rvm
+\curl -sSL https://get.rvm.io | bash -s stable
+
+#Reload config files
+source /etc/profile.d/rvm.sh
+
+#Install ruby 2.1
+rvm install ruby-2.1 --default
+
+#gem install bundler
+#gem install rails
+
